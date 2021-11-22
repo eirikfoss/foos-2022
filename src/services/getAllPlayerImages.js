e914a788aa0b4fe0121017bfe8dc7490cf7ed23c
eirikfoss <eirikfoss@gmail.com>
2021-11-22 23:23:03 +0100

    fixed access tokens

--- src/services/getAllPlayerImages.js
+++ src/services/getAllPlayerImages.js
@@ -1,9 +1,13 @@
 
+
 export default function getAllPlayerImages() {
+    require("dotenv").config();
+    const dpxAccess = process.env.REACT_APP_DPX_TOKEN;
+    
     let fetch = require('isomorphic-fetch');
     let Dropbox = require('dropbox').Dropbox;
     const path =  "";
-    let dbx = new Dropbox({ accessToken: "cjIr0_-l1IkAAAAAAAAAF15RI8jXaBQJeb-2tgTyhSlwnYInynbnpoxHOLyfILfO", fetch: fetch });
+    let dbx = new Dropbox({ accessToken: dpxAccess, fetch: fetch });
     return dbx.filesListFolder({path: path })
     
 }
--- src/services/getPlayerImg.js
+++ src/services/getPlayerImg.js
@@ -1,12 +1,15 @@
+
 export default function getplayerImages(username) {
-  const dpxAccess = process.env.DPX_TOKEN;
+  require("dotenv").config();
+  const dpxAccess = process.env.REACT_APP_DPX_TOKEN;
+  
 
   let fetch = require("isomorphic-fetch");
   let Dropbox = require("dropbox").Dropbox;
   const imgPath = "/" + username + ".jpeg";
   let dbx = new Dropbox({
     accessToken:
-      "cjIr0_-l1IkAAAAAAAAATyip01UKY7QYj04vxBvGes4OtKs-jlsIZcC4acHNG_JD",
+      dpxAccess,
     fetch: fetch
   });
   return dbx.filesDownload({ path: imgPath });
